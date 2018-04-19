48a1f5e265dddd36a136f84ad331b16e42055924
ryou <visualjupiter@gmail.com>
2018-04-19 19:13:06 +0900

    移動仮実装

---
+++ Assets/AgentController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentController : MonoBehaviour {
+    public Transform target;
+
+    private NavMeshAgent agent;
+    private Animator animator;
+
+    void Start()
+    {
+        this.agent = GetComponent<NavMeshAgent>();
+        this.animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        this.agent.SetDestination(target.position);
+        this.animator.SetFloat("Speed", this.agent.velocity.sqrMagnitude);
+    }
+}
